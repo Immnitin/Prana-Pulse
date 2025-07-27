d9c591ccdb1a50655b275180044c499eb6931bf4
mohammadazaruddinshaik <mdazaruddinthedeveloper@gmail.com>
2025-07-27 12:44:42 +0530

    codebase updated

---
+++ prana-pulse/src/firebase-config.js
@@ -0,0 +1,20 @@
+// src/firebase-config.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from "firebase/storage"; 
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6S8O4-JptT-joDl21tjqhOYn8uLQ543U",
+  authDomain: "my-app-d8e02.firebaseapp.com",
+  projectId: "my-app-d8e02",
+  storageBucket: "my-app-d8e02.firebasestorage.app",
+  messagingSenderId: "596473812476",
+  appId: "1:596473812476:web:3e863cc4f98560f1810e79",
+  measurementId: "G-2G7V2MR65X"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app); 
